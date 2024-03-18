9791832b6ebaf2be0dc75b5a0e1eeb278670cc4e
feramb18 <ferdinando.ambrosio003@studenti.uniparthenope.it>
2024-03-18 14:40:15 +0100

    aggiunte le istruzioni su come andare avanti e indietro

--- RicettarioW Watch App/StartW.swift
+++ RicettarioW Watch App/StartW.swift
@@ -4,31 +4,61 @@
 //
 //  Created by Carmine De Micco on 28/02/24.
 //
-
+import WatchKit
 import SwiftUI
 
-struct StartW: View {
-    var body: some View {
-        VStack{
-            Spacer()
+    struct StartW: View {
+        @State private var emoji = "🤏"
+        @State private var isNextEmoji = false
+        
+        let emojis = ["👌💥","🤏"] // Array di emoji da mostrare
+        
+        var body: some View {
+            VStack {
+                Spacer()
+                Text("Watch and Cook!")
+                    .font(.title3)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .padding(.top,0)
+                Spacer()
+                VStack {
+                    Spacer()
+                    Text("Tap your fingers to go to the next step.")
+                                        .opacity(0.7)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .font(.headline)
+                    Spacer()
+                    Text(emoji)
+                        .font(.title)
+                    Spacer()
+                    Text("Tap two times to go to the previous step")
+                        .font(.headline)
+                        .opacity(0.7)
+                        .fixedSize(horizontal: false, vertical: true)
+                        .padding(.bottom,0)
+                }
+            }
             
-            Text("Watch and Cook!")
-                .font(.title3)
-            
-            Spacer()
-            
-            Text("Tap your fingers to go to the next step.")
-                .opacity(0.7)
-            Text("💥👌🏻")
-                .font(.system(size: 35))
-            
-            Spacer()
+            .background(Color.black)
+            .padding()
+            .onAppear {
+                // Avvia il timer quando la vista appare
+                Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { timer in
+                    withAnimation {
+                        if self.isNextEmoji {
+                            let currentIndex = (self.emojis.firstIndex(of: self.emoji) ?? 0)
+                            let nextIndex = (currentIndex + 1) % self.emojis.count
+                            self.emoji = self.emojis[nextIndex]
+                        } else {
+                            let currentIndex = (self.emojis.firstIndex(of: self.emoji) ?? 0)
+                            let nextIndex = (currentIndex - 1 + self.emojis.count) % self.emojis.count
+                            self.emoji = self.emojis[nextIndex]
+                        }
+                    }
+                }
+            }
         }
-        .background(Color.black)
-        .padding()
     }
-    
-}
 
 #Preview {
     StartW()
