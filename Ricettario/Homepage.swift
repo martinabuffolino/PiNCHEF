4fd95ad4130076373a858a7fcaa85c95b7e7b42d
martinabuffolino <martina.buffolino002@studenti.uniparthenope.it>
2024-03-18 11:38:32 +0100

    nuova search bar

--- Ricettario/Homepage.swift
+++ Ricettario/Homepage.swift
@@ -3,7 +3,7 @@ import SwiftUI
 
 // Homepage
 struct HomeView: View {
-    @StateObject private var watchConnector:WatchConnector = WatchConnector()
+    @StateObject private var watchConnector: WatchConnector = WatchConnector()
     
     @Binding var isHeartRed: Bool
     @Binding var randomRecipes: [Recipe]
@@ -21,6 +21,8 @@ struct HomeView: View {
         NavigationStack {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(spacing: 0){
+                    Spacer()
+                    SearchBar(searchText: $searchText)
                     ScrollView(.horizontal, showsIndicators: false) {
                         HStack {
                             CategoryButton(title: "Appetizers", imageName: "appetizers", destination: RecipeCategoryView(isHeartRed: $isHeartRed, recipeManager: recipeManager, recipes: appetizersRecipes, category: .appetizers))
@@ -31,23 +33,31 @@ struct HomeView: View {
                     }
                     
                     Text(searchText.isEmpty ? "RECIPES OF THE DAY" : "RESULTS").font(.title).fontWeight(.semibold).padding(10)
-                    VStack(spacing: 20) {
-                        ForEach(searchText.isEmpty ? randomRecipes : filteredRecipes) { recipe in
-                            RecipeCard(recipe: recipe, isHeartRed: self.isRecipeSaved(recipe), toggleHeart: {
-                                if self.isRecipeSaved(recipe) {
-                                    self.recipeManager.deleteRecipe(recipe)
-                                } else {
-                                    self.recipeManager.addRecipe(recipe)
-                                }
-                            })
+                    
+                    if filteredRecipes.isEmpty && !searchText.isEmpty {
+                        Text("No results found for '\(searchText)'")
+                            .foregroundColor(.black)
+                            .padding(.bottom, 10)
+                    } else {
+                        VStack(spacing: 20) {
+                            ForEach(searchText.isEmpty ? randomRecipes : filteredRecipes) { recipe in
+                                RecipeCard(recipe: recipe, isHeartRed: self.isRecipeSaved(recipe), toggleHeart: {
+                                    if self.isRecipeSaved(recipe) {
+                                        self.recipeManager.deleteRecipe(recipe)
+                                    } else {
+                                        self.recipeManager.addRecipe(recipe)
+                                    }
+                                })
+                            }
                         }
-                    }.buttonStyle(PlainButtonStyle())
+                        .buttonStyle(PlainButtonStyle())
+                    }
                 }
-            }.background(Image("background").resizable().scaledToFill())
-                .navigationTitle("Recipe Book")
-                .toolbarBackground(.yellow, for: .navigationBar)
-                .toolbarBackground(.visible, for: .navigationBar)
-                .searchable(text: $searchText)
+            }
+            .background(Image("background").resizable().scaledToFill())
+            .navigationTitle("Recipe Book")
+            .toolbarBackground(.yellow, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
 
         }
         .tint(.black)
